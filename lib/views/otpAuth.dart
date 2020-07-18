4eb2fb3d876c1fd2352f030663c67ad0399fe270
Hyper-Programmer <hyperprogrammeri@gmail.com>
2020-07-18 16:35:44 +0530

    4th Commit: Added Firebase Phone Authentication Function, Changes minSDK from 16 to 21.

--- lib/views/otpAuth.dart
+++ lib/views/otpAuth.dart
@@ -1,123 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:notespedia/views/authTosWidget.dart';
 import 'background_1.dart';
+import 'homeView.dart';
 
 //OTP Auth Main
 class otpAuth extends StatelessWidget {
+
+  //  Variable Declarations
+  final otpTextEditController = TextEditingController();
+  dynamic verificationId, sharedOtpVal, _otpVal;
+  final _otpFormKey = GlobalKey<FormState>();
+
+  otpAuth({@required this.verificationId});
+
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: otpAuthBody(),
+        body: otpAuthBody(context),
       ),
     );
   }
-}
 
 // OTP Auth Body
-class otpAuthBody extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+  otpAuthBody(BuildContext context) {
     return Center(
       child: Stack(
         children: <Widget>[
           background("assets/images/otpIllustration.png"),
-      SingleChildScrollView(
-        child: Container(
-            margin: EdgeInsets.fromLTRB(0, 200, 0, 0),
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                child: Card(
-                  elevation: 20.0,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Image(
-                          image: AssetImage("assets/images/logo.png"),
-                          height: 150,
-                          width: 150,
-                        ),
-                        Text(
-                          "Enter OTP, Send to your Phone Number.",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontFamily: "font_primary",
-                            color: Colors.grey[500],
-                          ),
-                        ),
-                        Form(
-                          child: TextFormField(
-                            // ignore: missing_return
-                            validator: (String value) {
-                              if (value.isEmpty) {
-                                return "Required!";
-                              } else if (value.length < 10) {
-                                return "Invalid Phone Number!";
-                              }
-                            },
-                            maxLength: 6,
-                            maxLines: 1,
-                            keyboardType: TextInputType.phone,
-                            textAlign: TextAlign.center,
-                            decoration: InputDecoration(
-                              hintText: "* * * *",
-                              prefixIcon: Icon(
-                                Icons.vpn_key,
-                                color: Colors.lightBlue,
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(4)),
-                                borderSide:
-                                BorderSide(width: 2, color: Colors.lightBlue),
-                              ),
-                              border: OutlineInputBorder(),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        SizedBox(
-                          height: 40,
-                          width: 400,
-                          child: RaisedButton(
-                              color: Colors.lightBlue,
-                              onPressed: () {},
-                              child: Text(
-                                "Verify OTP",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: "font_primary"),
-                              )),
-                        ),
-                        Divider(
-                          color: Colors.lightBlue,
-                          height: 20,
-                        ),
-                        authTosWidget(),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            )),
-      ),
+          otpAuthCard(context),
         ],
       ),
     );
   }
-}
 
-// OTP Auth Elevated Card
-class otpAuthCard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+//  OTP Auth Card
+  otpAuthCard(BuildContext context) {
     return SingleChildScrollView(
       child: Container(
           margin: EdgeInsets.fromLTRB(0, 200, 0, 0),
@@ -146,13 +68,15 @@ class otpAuthCard extends StatelessWidget {
                         ),
                       ),
                       Form(
+                        key: _otpFormKey,
                         child: TextFormField(
+                          controller: otpTextEditController,
                           // ignore: missing_return
                           validator: (String value) {
                             if (value.isEmpty) {
                               return "Required!";
-                            } else if (value.length < 10) {
-                              return "Invalid Phone Number!";
+                            } else if (value.length < 6) {
+                              return "Invalid OTP Code!";
                             }
                           },
                           maxLength: 6,
@@ -166,7 +90,8 @@ class otpAuthCard extends StatelessWidget {
                               color: Colors.lightBlue,
                             ),
                             enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(4)),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4)),
                               borderSide:
                                   BorderSide(width: 2, color: Colors.lightBlue),
                             ),
@@ -182,7 +107,25 @@ class otpAuthCard extends StatelessWidget {
                         width: 400,
                         child: RaisedButton(
                             color: Colors.lightBlue,
-                            onPressed: () {},
+                            onPressed: () async {
+                              if (_otpFormKey.currentState.validate()) {
+                                _otpVal = otpTextEditController.text.trim();
+                                AuthCredential authcredential = PhoneAuthProvider
+                                    .getCredential(
+                                    verificationId: verificationId, smsCode: _otpVal);
+                                AuthResult authresult = await FirebaseAuth.instance
+                                    .signInWithCredential(authcredential);
+                                FirebaseUser firebaseuser = authresult.user;
+
+                                if (firebaseuser != null) {
+                                  Navigator.push(context, MaterialPageRoute(
+                                      builder: (context) => homeView()
+                                  ));
+                                } else {
+                                  print("Error: Error on navigating to home.");
+                                }
+                              }
+                            },
                             child: Text(
                               "Verify OTP",
                               style: TextStyle(
@@ -204,3 +147,114 @@ class otpAuthCard extends StatelessWidget {
     );
   }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+// OTP Auth Body
+//class otpAuthBody extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Center(
+//      child: Stack(
+//        children: <Widget>[
+//          background("assets/images/otpIllustration.png"),
+//          otpAuthCard(),
+//        ],
+//      ),
+//    );
+//  }
+//}
+
+// OTP Auth Elevated Card
+//class otpAuthCard extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return SingleChildScrollView(
+//      child: Container(
+//          margin: EdgeInsets.fromLTRB(0, 200, 0, 0),
+//          child: Center(
+//            child: Padding(
+//              padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+//              child: Card(
+//                elevation: 20.0,
+//                child: Padding(
+//                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+//                  child: Column(
+//                    mainAxisSize: MainAxisSize.min,
+//                    children: <Widget>[
+//                      Image(
+//                        image: AssetImage("assets/images/logo.png"),
+//                        height: 150,
+//                        width: 150,
+//                      ),
+//                      Text(
+//                        "Enter OTP, Send to your Phone Number.",
+//                        textAlign: TextAlign.center,
+//                        style: TextStyle(
+//                          fontSize: 14,
+//                          fontFamily: "font_primary",
+//                          color: Colors.grey[500],
+//                        ),
+//                      ),
+//                      Form(
+//                        child: TextFormField(
+//                          // ignore: missing_return
+//                          validator: (String value) {
+//                            if (value.isEmpty) {
+//                              return "Required!";
+//                            } else if (value.length < 10) {
+//                              return "Invalid Phone Number!";
+//                            }
+//                          },
+//                          maxLength: 6,
+//                          maxLines: 1,
+//                          keyboardType: TextInputType.phone,
+//                          textAlign: TextAlign.center,
+//                          decoration: InputDecoration(
+//                            hintText: "* * * *",
+//                            prefixIcon: Icon(
+//                              Icons.vpn_key,
+//                              color: Colors.lightBlue,
+//                            ),
+//                            enabledBorder: OutlineInputBorder(
+//                              borderRadius: BorderRadius.all(Radius.circular(4)),
+//                              borderSide:
+//                                  BorderSide(width: 2, color: Colors.lightBlue),
+//                            ),
+//                            border: OutlineInputBorder(),
+//                          ),
+//                        ),
+//                      ),
+//                      SizedBox(
+//                        height: 10,
+//                      ),
+//                      SizedBox(
+//                        height: 40,
+//                        width: 400,
+//                        child: RaisedButton(
+//                            color: Colors.lightBlue,
+//                            onPressed: () {},
+//                            child: Text(
+//                              "Verify OTP",
+//                              style: TextStyle(
+//                                  color: Colors.white,
+//                                  fontFamily: "font_primary"),
+//                            )),
+//                      ),
+//                      Divider(
+//                        color: Colors.lightBlue,
+//                        height: 20,
+//                      ),
+//                      authTosWidget(),
+//                    ],
+//                  ),
+//                ),
+//              ),
+//            ),
+//          )),
+//    );
+//  }
+//}
--- lib/views/phoneAuth.dart
+++ lib/views/phoneAuth.dart
@@ -1,172 +1,218 @@
-library progress_dialog;
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:notespedia/views/authTosWidget.dart';
 import 'package:notespedia/views/profileBuild.dart';
 import 'background_1.dart';
+import 'background_1.dart';
+import 'homeView.dart';
+import 'otpAuth.dart';
+
+
 
 //Phone Auth Main
-// ignore: camel_case_types
 class phoneAuth extends StatelessWidget {
+
+  //  Variable Declarations
+  final _phoneNumberTextEditController = TextEditingController();
+  dynamic phoneNumberVal;
+  final _phoneFormKey = GlobalKey<FormState>();
+
+
+
+
+
+  // Authentication
+  Future <bool> Authentication(String phoneNumber, BuildContext context) async{
+    FirebaseAuth _fAuth = FirebaseAuth.instance;
+
+    _fAuth.verifyPhoneNumber(
+
+        //Phone Number
+        phoneNumber: phoneNumber,
+
+        // Timeout Duration
+        timeout: Duration(seconds: 60),
+
+        //Verification Completed
+        verificationCompleted: (AuthCredential credential) async {
+          AuthResult authResult = await FirebaseAuth.instance.signInWithCredential(credential);
+          FirebaseUser firebaseUser = authResult.user;
+          if (firebaseUser != null) {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => homeView()));
+          }
+        },
+
+        //Verification Failed
+        verificationFailed: (AuthException authexception){
+          print(authexception.message);
+        },
+
+        //On CodeSent to Phone Number
+        codeSent: (String verificationId, [int forceResendingToken]){
+
+          Navigator.push(context, MaterialPageRoute(
+              builder: (context) => otpAuth(verificationId: verificationId,)
+          ));
+
+        },
+        codeAutoRetrievalTimeout: null);
+
+  }
+
+
+
+
+
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: phoneAuthBody(),
+        body: phoneAuthBody(context),
       ),
     );
   }
-}
-
-// Phone Auth Body
-class phoneAuthBody extends StatefulWidget {
-  @override
-  _phoneAuthBodyState createState() => _phoneAuthBodyState();
-}
-
-class _phoneAuthBodyState extends State<phoneAuthBody> {
 
 
-
-
-  bool _loading;
-  @override
-  void initState() {
-    super.initState();
-    _loading = false;
-  }
-
-
-
-
-
-  @override
-  Widget build(BuildContext context) {
+//  Phone Auth Body
+  phoneAuthBody(BuildContext context) {
     return Center(
       child: Stack(
         children: <Widget>[
-          Container(
-            child: Column(
-              children: <Widget>[
-                _loading
-                    ? LinearProgressIndicator()
-                    : SizedBox(height: 1,),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  color: Colors.white,
-                  height: 300,
-                  child: Image(
-                    image: AssetImage("assets/images/phoneAuth_illustration.jpg"),
-                    height: 200,
-                    width: 200,
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    color: Colors.lightBlue,
-                  ),
-                )
-              ],
-            ),
-          ),
-          SingleChildScrollView(
-            child: Container(
-                margin: EdgeInsets.fromLTRB(0, 200, 0, 0),
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                    child: Card(
-                      elevation: 20.0,
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            Image(
-                              image: AssetImage("assets/images/logo.png"),
-                              height: 150,
-                              width: 150,
-                            ),
-                            Text(
-                              "Verify your Phone Number.",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontFamily: "font_primary",
-                                color: Colors.grey[500],
-                              ),
-                            ),
-                            Form(
-                              child: TextFormField(
-                                // ignore: missing_return
-                                validator: (String value) {
-                                  if (value.isEmpty) {
-                                    return "Required!";
-                                  } else if (value.length < 10) {
-                                    return "Invalid Phone Number!";
-                                  }
-                                },
-                                maxLength: 10,
-                                maxLines: 1,
-                                keyboardType: TextInputType.phone,
-                                decoration: InputDecoration(
-                                  hintText: "+91",
-                                  prefixIcon: Icon(
-                                    Icons.phone,
-                                    color: Colors.lightBlue,
-                                  ),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(4)),
-                                    borderSide: BorderSide(
-                                        width: 2, color: Colors.lightBlue),
-                                  ),
-                                  border: OutlineInputBorder(),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            SizedBox(
-                              height: 40,
-                              width: 400,
-                              child: RaisedButton(
-                                  color: Colors.lightBlue,
-                                  onPressed: () {
-                                    setState(() {
-                                      _loading = !_loading;
-                                    });
-                                    //Navigator.of(context).push(MaterialPageRoute(builder: (context) => profileBuild()));
-                                  },
-                                  child: Text(
-                                    "Get OTP",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontFamily: "font_primary"),
-                                  )),
-                            ),
-                            Divider(
-                              color: Colors.lightBlue,
-                              height: 20,
-                            ),
-                            authTosWidget(),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                )),
-          ),
+          background("assets/images/phoneAuth_illustration.jpg"),
+          phoneAuthCard(context),
         ],
       ),
     );
   }
+
+//  Phone Auth Card
+  phoneAuthCard(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+          margin: EdgeInsets.fromLTRB(0, 200, 0, 0),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: Card(
+                elevation: 20.0,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Image(
+                        image: AssetImage("assets/images/logo.png"),
+                        height: 150,
+                        width: 150,
+                      ),
+                      Text(
+                        "Verify your Phone Number.",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontFamily: "font_primary",
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                      Form(
+                        key: _phoneFormKey,
+                        child: TextFormField(
+                          controller: _phoneNumberTextEditController,
+                          // ignore: missing_return
+                          validator: (String value) {
+                            if (value.isEmpty) {
+                              return "Required!";
+                            } else if (value.length < 10) {
+                              return "Invalid Phone Number!";
+                            }
+                          },
+                          maxLength: 10,
+                          maxLines: 1,
+                          keyboardType: TextInputType.phone,
+                          decoration: InputDecoration(
+                            hintText: "+91",
+                            prefixIcon: Icon(
+                              Icons.phone,
+                              color: Colors.lightBlue,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4)),
+                              borderSide:
+                                  BorderSide(width: 2, color: Colors.lightBlue),
+                            ),
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        height: 40,
+                        width: 400,
+                        child: RaisedButton(
+                            color: Colors.lightBlue,
+                            onPressed: () {
+                              if (_phoneFormKey.currentState.validate()) {
+                                phoneNumberVal = "+91" + _phoneNumberTextEditController.text.trim();
+                                Authentication(phoneNumberVal, context);
+                              }
+                            },
+                            child: Text(
+                              "Get OTP",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: "font_primary"),
+                            )),
+                      ),
+                      Divider(
+                        color: Colors.lightBlue,
+                        height: 20,
+                      ),
+                      authTosWidget(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )),
+    );
+  }
 }
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+//// Phone Auth Body
+//class phoneAuthBody extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Center(
+//      child: Stack(
+//        children: <Widget>[
+//          background("assets/images/phoneAuth_illustration.jpg"),
+//          phoneAuthCard(),
+//        ],
+//      ),
+//    );
+//  }
+//}
 
 // Phone Auth Elevated Card
 //class phoneAuthCard extends StatefulWidget {
